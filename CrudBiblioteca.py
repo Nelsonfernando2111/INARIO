05071129d4d455c12661833c4b28a79fd8f99947
Nelsonfernando2111 <nelsonfernando2111@gmail.com>
2025-08-12 09:20:19 -0300

    Update CrudBiblioteca.py

--- CrudBiblioteca.py
+++ CrudBiblioteca.py
@@ -29,20 +29,22 @@ class Biblioteca:
             for livro in self.livros:
                 print(livro)
 
-    def actualizarLivro(self, id, novo_titulo=None, novo_autor=None, nova_categoria=None, novo_status=None):
+    def actualizarLivro(self, id, novo_titulo=None, novo_autor=None, novo_ano=None, nova_categoria=None, novo_status=None):
         for livro in self.livros:
             if livro._id==id:
                 if novo_titulo:
-                    livro.titulo=novo_titulo
+                    livro._titulo=novo_titulo
                 if novo_autor:
-                    livro.autor=novo_autor
+                    livro._autor=novo_autor
+                if novo_ano:
+                    livro._ano=novo_ano
                 if nova_categoria:
-                    livro.categoria=nova_categoria
-                if novos_tatus:
-                    livro.status=novo_status
+                    livro._categoria=nova_categoria
+                if novo_status:
+                    livro._status=novo_status
                 print("Livro Actualizado com sucesso")
                 return
-                print("Livro nao Encontrado")
+        print("Livro nao Encontrado")
 
     def DeletarLivro(self, id):
         for livro in self.livros:
@@ -50,7 +52,7 @@ class Biblioteca:
                 self.livros.remove(livro)
                 print(f"Livro {livro._titulo} removido com sucesso")
                 return
-                print("Livro nao Encontrado.")
+        print("Livro nao Encontrado.")
 
 
 def menu():
@@ -59,7 +61,7 @@ def menu():
     while True:
         print("Bem vindo ao Sistema da Biblioteca")
 
-        print("1-Adicioanar Livros")
+        print("1-Adicionar Livros")
         print("2-Listar Livros")
         print("3-Actualizar Livros")
         print("4-Remover Livro")
@@ -77,12 +79,12 @@ def menu():
             biblioteca.listarLivros()
         elif opcao == "3":
             id_livro=int(input("ID do livro a Actualizar:"))
-            titulo=input("Titulo do livro a Actualizar:")
-            autor=input("Nome do Autor do livro a Actualizar:")
-            ano=input("Ano do livro a Actualizar:")
-            novacategoria=input("Nova Categoriar: ") or None
-            status=input("Novo Status: ") or None
-            biblioteca.actualizarLivro(id_livro,titulo,autor, ano, categoria, status)
+            novo_titulo=input("Novo Titulo:")
+            novo_autor=input("Nome do Autor:")
+            novo_ano=input("Ano: ")
+            nova_categoria=input("Nova Categoriar: ") or None
+            novo_status=input("Novo Status: ") or None
+            biblioteca.actualizarLivro(id_livro, novo_titulo, novo_autor, novo_ano, nova_categoria, novo_status)
 
         elif opcao == "4":
             id_livro=int(input("ID do Livro a Remover: "))
